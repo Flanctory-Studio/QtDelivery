1e9679c8239424fb6d4b9a8e52989ea3a8d853c4
Óscar Hernández <oscarhernandezgomez@gmail.com>
2020-03-15 17:10:44 +0100

    DrawGameObject()

--- Project/gameobject.h
+++ Project/gameobject.h
@@ -3,7 +3,17 @@
 
 #include <QPen>
 #include <QColor>
-#include <QWidget>
+
+
+class SceneWidget;
+
+enum class Shape
+{
+    Square,
+    Triangle,
+    Circle
+};
+
 
 class GameObject
 {
@@ -14,13 +24,17 @@ public:
     float* position = new float[3] {0,0,0};
     float* scale = new float[3] {1, 1, 1};
 
-    Qt::PenCapStyle borderStyle = Qt::PenCapStyle::FlatCap;
+    Qt::PenStyle borderStyle = Qt::PenStyle::SolidLine;
     QColor borderColor = QColorConstants::Black;
+    int lineSize = 10;
 
     Qt::BrushStyle shapeStyle = Qt::BrushStyle::SolidPattern;
     QColor shapeColor = QColorConstants::White;
 
-    void Paint(QWidget* centralWidget);
+    Shape shape = Shape::Square;
+
+public:
+    void Paint(SceneWidget* screen);
 };
 
 #endif // GAMEOBJECT_H
